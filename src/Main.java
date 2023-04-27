7efe6fc8f79f836b8fc5a47f2686bbeae8b05be2
Alex Nohrin <beterok@inbox.ru>
2023-04-27 20:28:39 +0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+
+       int price = 12868;
+       int accrual = 20;
+       int bonus = 1;
+
+       int miles = price / accrual * bonus;
+       System.out.println("Количество начисленных миль за купленный билет: " + miles);
+    }
+}
